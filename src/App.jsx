7007f6089c0fd462feecb203e0c5a1adb889371d
Muhammad Osama <m.osama169@gmail.com>
2023-03-29 22:59:43 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {Home} from './pages/Home'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+import {PageNotFound} from './components/PageNotFound/PageNotFound'
+import {MovieDetails} from './components/MovieDetails/MovieDetails'
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/movie/:imdbID" element={<MovieDetails/>}/>
+          <Route element={<PageNotFound/>}/>
+        </Routes>
     </Router>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useDispatch} from "react-redux"
+import {MovieListing} from '../components/MovieListing/MovieListing'
+import movieApi from '../apis/movieApi'
+import { addMovies } from '../store/movieSlice'
+
 
 export const Home = () => {
+  
+  const dispactch = useDispatch();
+
+  useEffect(()=>{
+    const movieText = "harry";
+
+    const fetchMovie =async () =>{
+      const response = await movieApi.get(
+        `?apikey=${import.meta.env.VITE_API_KEY}&s=${movieText}&type=movie`
+        )
+        .catch((err) =>{
+          console.log(err)
+        })
+        dispactch(addMovies(response.data))
+    }
+    fetchMovie()
+  },[])
+
   return (
-    <div>Home</div>
+    <div>
+      Working
+      <MovieListing/>
+    </div>
   )
 }
